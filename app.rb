6b0ed90bf20f1f4ffd4e021fd6dae4521a656582
giriraj0209 <giriraj.kashyapa@gmail.com>
2016-02-27 22:41:29 -0600

    Initial Commit

---
+++ app.rb
@@ -0,0 +1,13 @@
+class App < Sinatra::Base
+	
+	get "/" do
+		@images = Image.all
+		haml :index
+	end
+
+	post "/images" do
+	@image = Image.new params[:image]
+    @image.save
+    redirect "/"
+  end
+end
